0f5fa122feb01a93a2c81a907efab426662fe1fa
Fernando Meira <thomwork@outlook.com>
2022-04-05 21:57:04 -0300

    hotfix: solved imports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,3 @@ export { default as Post } from './Post';
 export { default as Input } from './Input';
 export { default as Header } from './Header';
 export { default as Button } from './Button';
-export { default as StatusFooter } from './StatusFooter';
