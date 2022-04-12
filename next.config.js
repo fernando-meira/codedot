d23647dcbabfb647ef96941ef9673f60cfa43ed9
Fernando Meira <thomwork@outlook.com>
2022-04-11 21:45:19 -0300

    feat: image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  images: {
+    domains: ['images.prismic.io', 'code-dot.cdn.prismic.io'],
+  },
 };
--- src/pages/posts/[slug].page.tsx
+++ src/pages/posts/[slug].page.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import { RichText } from 'prismic-dom';
 import { GetStaticPaths, GetStaticProps } from 'next';
 
@@ -7,9 +8,18 @@ import { createClient } from '~/../prismicio';
 
 import * as S from './slug.styles';
 
+interface ImageData {
+  dimensions?: {
+    width?: number;
+    height?: number;
+  };
+  alt?: string;
+  url?: string;
+}
 interface Post {
   slug: string;
   title: string;
+  image?: ImageData;
   content: React.ReactNode;
   updatedAt: string | Date;
 }
@@ -31,6 +41,17 @@ export default function Post({ post }: PostProps) {
         <Header />
 
         <S.Section>
+          {post.image && (
+            <div>
+              <Image
+                src={post.image.url}
+                alt={post.image.alt}
+                width={post.image.dimensions.width}
+                height={post.image.dimensions.height}
+              />
+            </div>
+          )}
+
           <Title content={post.title} />
 
           <S.ContentWrapper>
@@ -65,6 +86,14 @@ export const getStaticProps: GetStaticProps = async ({
 
   const post = {
     slug,
+    image: {
+      dimensions: {
+        width: response.data.default_image.dimensions.width,
+        height: response.data.default_image.dimensions.height,
+      },
+      alt: response.data.default_image.alt ?? '',
+      url: response.data.default_image.url ?? undefined,
+    },
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content),
     updatedAt: new Date(response.last_publication_date).toLocaleDateString(
